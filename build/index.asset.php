4e27a6fd15c58945bcf5d925d7336998c9805f18
Avijit karmaker <ajkacca@gmail.com>
2023-08-01 12:29:44 +0300

    add custom typography

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b72d607276a07fbab772');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dd810f9cdee12bffb8c2');
