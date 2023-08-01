96f0db4c81782921cf983a3e05ac8c903485ae22
Avijit karmaker <ajkacca@gmail.com>
2023-08-01 12:33:40 +0300

    update block properties

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dd810f9cdee12bffb8c2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '972e8d533d31fb83f58a');
