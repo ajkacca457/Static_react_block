c43d9513bf9817db6dd47e6dbc78dc00fa28bdd7
Avijit Karmaker <ajkacca@gmail.com>
2023-08-06 00:24:35 +0300

    add default colors to block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b0ec5ed394b967ac4691');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'bc1ccdb02699d68a108f');
