bfb3615b93f7ab1e158cc80e9170866e70659703
Avijit Karmaker <ajkacca@gmail.com>
2023-08-05 23:42:48 +0300

    remove custom styling classes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6a7715755d9a65cdef55');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b0ec5ed394b967ac4691');
